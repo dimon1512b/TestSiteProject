eb0506f486c7accf62cb37a70e59b83a8f00b91f
Dmytry Bezryk <espado1512b@gmail.com>
2021-05-25 16:58:02 +0300

    We have a panel for filtration that is located to the left. The main page displays cards with all cars

--- mysite/cars/views.py
+++ mysite/cars/views.py
@@ -1,3 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Cars
 
-# Create your views here.
+
+class BasePage:
+
+	def show_page(self):
+		data = Cars.objects.order_by('date_created')
+		paginator = Paginator(data, 20)
+		page_number = self.GET.get('page')
+		page_obj = paginator.get_page(page_number)
+		return render(self, 'cars/base.html', {'data': data, 'page_obj': page_obj})
